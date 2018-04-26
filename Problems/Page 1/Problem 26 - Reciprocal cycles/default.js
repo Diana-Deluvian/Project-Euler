7686b4e1a4e070d738e1cd6b44dbf060001e8159
Diana-Deluvian <diana.deluvian@gmail.com>
2018-04-26 14:26:21 +0200

    Problem 26

--- Problems/Page 1/Problem 26 - Reciprocal cycles/default.js
+++ Problems/Page 1/Problem 26 - Reciprocal cycles/default.js
@@ -7,75 +7,31 @@ $( "#runSolution" ).click(function() {
 //write code for calculations below
 
 function reciprocalCycles(x){
-	var max=0;
-	for(var i = 2; i < x; i++){
-		var temp = 0;
-		temp = findRepetition(Divide(i)).length;
-		if(max < temp ){
-			max = temp;
-			console.log(i);
-			console.log(Divide(i));
-		}  
-	}
+var sequenceLength = 0;
+// we check the unique number we get with every division since at one point they will start repeating
+// the length of repetition can only be as long as there are unique combinations of numbers to divide, therefore we dont need to check the numbers once sequenceLength
+// becomes larger than i
+ 
+for (var i = 1000; i > 1; i--) {
+    if (sequenceLength >= i) {
+        break;
+    }
+ 
+    var foundRemainders = new Array(i);
+    foundRemainders.fill(0);
+    var value = 1;
+    var position = 0;
+ 
+    while (foundRemainders[value] == 0 && value != 0) {
+        foundRemainders[value] = position;
+        value *= 10;
+        value %= i;
+        position++;
+    }
+ 
+    if (position - foundRemainders[value] > sequenceLength) {
+        sequenceLength = position - foundRemainders[value];
+    }
+    return i;
 }
-	
-
-function Divide(x){
-	var result = [];
-	var divisor = 1;
-	var tempNum = 0;
-	for(; result.length < 1000;){
-		tempNum = Math.floor(divisor/x);
-		if(tempNum){
-			result.push(tempNum);
-			divisor -= tempNum*x;
-			if(!divisor) break;
-			tempNum = 0;
-		}
-		else {
-			divisor*=10;
-		}
-	}
-	return result;
-
 }
-
-function findRepetition(x){
-	if(x.length < 1000) return 0;
-	var firstDigit = x[0];
-	var cpy = x.slice(1);
-	var loc, loc2;
-	var arr1, arr2 = [];
-	arr1 = findInstance(cpy, firstDigit);
-	cpy = cpy.slice(arr1.length);
-	arr2 = findInstance(cpy, firstDigit);
-	if(checkArrs(arr1, arr2))
-	return arr1;
-return 0;
-}
-
-function findInstance(x, digit) {
-	return x.slice(0, x.indexOf(digit) +1);
-}
-
-function checkArrs(arr1, arr2){
-	if(arr1.length==arr2.length && arr1.every((v,i)=> v === arr2[i])) return 1;
-	return 0;
-}
-
-
-
-/*	for(var i = x.length / 2; i < cpy.length;){
-		loc = cpy.indexOf(firstDigit);
-		cpy = cpy.slice(loc);
-		arr1 = x.slice(0, loc+1);
-		loc2 = cpy.indexOf(firstDigit);
-		arr2 = x.slice(loc+1, loc2+1);
-		console.log(cpy, arr1, arr2);
-		if(arr1.length==arr2.length && arr1.every((v,i)=> v === arr2[i])) break;
-	}
-	return arr1;
-}
-*/
-
-// Maybe a recursive function ?
